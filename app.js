40899c6ef316da4af010789c70bab014e60fcbc5
muhammedirfanc <muhemmedirfan.sics@gmail.com>
2021-09-05 21:41:34 +0530

    added one more route and changed workflow to restart app

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import router from "./routes/routes.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/", (req, res) => res.json({ status: "ok" }));
+app.use("/api", router);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => res.json({ status: "ok" }).status(200));
+
+export default router;
