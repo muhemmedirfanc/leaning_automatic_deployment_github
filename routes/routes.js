8ef26ed5aba6f92a2dcf6c76b42f4fbccaedbb56
muhammedirfanc <muhemmedirfan.sics@gmail.com>
2021-09-05 22:42:23 +0530

    added new route to say hello 😜

--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,6 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => res.json({ status: "ok", message: "api is running." }).status(200));
+router.get("/say-hello", (req, res) => res.json({ status: "ok", message: "Hello !." }));
 
 export default router;
