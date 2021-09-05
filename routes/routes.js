304eaa0fb78a34ed2381054120a265fb5721e85c
muhammedirfanc <muhemmedirfan.sics@gmail.com>
2021-09-05 21:59:46 +0530

    test

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.json({ status: "ok" }).status(200));
+router.get("/", (req, res) => res.json({ status: "ok", message: "api is running." }).status(200));
 
 export default router;
